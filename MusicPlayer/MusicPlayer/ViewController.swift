8e06d99a4d986ab0ae582d55852800af5a537424
jisu park <qkrwltn8976@naver.com>
2019-07-21 23:24:26 +0900

    added codes

--- MusicPlayer/MusicPlayer/ViewController.swift
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -23,7 +23,23 @@ class ViewController: UIViewController {
         
     }
 
-
+    
+    @IBAction func playBtnAction(_ sender: Any) {
+        if !playBtn.isSelected {
+            player.play()
+            startTimer()
+        } else {
+            player.pause()
+            stopTimer()
+        }
+    }
+    
+    @IBAction func progressSliderAction(_ sender: UISlider) {
+        self.updateTimeLabel(time: TimeInterval(sender.value))
+        if sender.isTracking { return }
+        self.player.currentTime = TimeInterval(sender.value)
+    }
+    
 }
 
 extension ViewController: AVAudioPlayerDelegate {
@@ -36,11 +52,42 @@ extension ViewController: AVAudioPlayerDelegate {
             try self.player = AVAudioPlayer(data: sound.data)
             self.player.delegate = self
         } catch let err as NSError {
-            print("플래이어 초기화를 실패했습ㄴ디ㅏ")
-            print("Code: \(err.code), message: \(err.localizedDescription)")
+            print("플래이어 초기화를 실패했습니다")
+            print("코드: \(err.code), 메세지: \(err.localizedDescription)")
         }
         progressSlider.maximumValue = Float(self.player.duration)
         progressSlider.minimumValue = 0
+        progressSlider.value = Float(self.player.currentTime)
+    }
+    
+    func updateTimeLabel(time: TimeInterval) {
+        
+        let minute: Int = Int(time / 60)
+        let second: Int = Int(time.truncatingRemainder(dividingBy: 60))
+        let milisecond: Int = Int(time.truncatingRemainder(dividingBy: 1) * 100)
+
+        let timeText: String = String(format: "%02ld:%02ld:%02ld", minute, second, milisecond)
+
+        self.timeLabel.text = timeText
+    }
+    
+    func startTimer() {
+        self.timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { [unowned self] (timer: Timer) in
+
+            
+            if self.progressSlider.isTracking { return }
+            
+            self.updateTimeLabel(time: self.player.currentTime)
+            self.progressSlider.value = Float(self.player.currentTime)
+            
+        })
+        
+        self.timer.fire()
+    }
+    
+    func stopTimer() {
+        self.timer.invalidate()
+        self.timer = nil
     }
 }
 
