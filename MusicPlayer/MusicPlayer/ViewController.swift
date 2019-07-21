4519ebe9d2418d6aee5528ece5a2feab3d88e172
jisu park <qkrwltn8976@naver.com>
2019-07-21 20:44:41 +0900

    main page

--- MusicPlayer/MusicPlayer/ViewController.swift
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    var player: AVAudioPlayer!
+    var timer: Timer!
+    
+    @IBOutlet weak var progressSlider: UISlider!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var timeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initializePlayer()
+        
     }
 
 
+}
+
+extension ViewController: AVAudioPlayerDelegate {
+    func initializePlayer(){
+        guard let sound = NSDataAsset(name: "sound")
+            else {print("음원 파일이 존재하지 않습니다")
+            return
+        }
+        do {
+            try self.player = AVAudioPlayer(data: sound.data)
+            self.player.delegate = self
+        } catch let err as NSError {
+            print("플래이어 초기화를 실패했습ㄴ디ㅏ")
+            print("Code: \(err.code), message: \(err.localizedDescription)")
+        }
+        progressSlider.maximumValue = Float(self.player.duration)
+        progressSlider.minimumValue = 0
+    }
 }
 
